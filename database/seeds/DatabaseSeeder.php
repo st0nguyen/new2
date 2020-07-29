128bbd576feffdcad9a9768ac268965b1162cc54
st0nguyen <ndnguyen9111@gmail.com>
2020-07-29 09:44:21 +0700

    e3

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        $this->call(UsersTableSeeder::class);
-//        $this->call(TheLoaiTableSeeder::class);
-//        $this->call(LoaiTinTableSeeder::class);
-//        $this->call(TinTucTableSeeder::class);
-//        $this->call(CommentTableSeeder::class);
+       $this->call(UsersTableSeeder::class);
+       $this->call(TheLoaiTableSeeder::class);
+       $this->call(LoaiTinTableSeeder::class);
+       $this->call(TinTucTableSeeder::class);
+       $this->call(CommentTableSeeder::class);
     }
 }
