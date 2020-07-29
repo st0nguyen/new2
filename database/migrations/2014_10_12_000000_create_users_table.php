585433787212fa6dc9702a22adc051af208aa818
st0nguyen <ndnguyen9111@gmail.com>
2020-07-29 11:04:25 +0700

    x2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('idComment');
+            $table->integer('idComment')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
